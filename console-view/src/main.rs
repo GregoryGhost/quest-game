aff786a654a4941d60c944e5511c5a5a38b3ffe0
Gregory <lobster287@gmail.com>
2019-10-22 01:13:19 +0700

    #3 Добавил логирование в терминальной морде

--- console-view/src/main.rs
+++ console-view/src/main.rs
@@ -5,7 +5,7 @@ use logger::init_logger;
 pub mod logger;
 
 extern crate parser_graphml;
-use parser_graphml::parser::{read_graphml, Vertex, Edge};
+use parser_graphml::parser::{read_graphml, Edge, Vertex};
 
 use petgraph::graph::EdgeReference;
 use petgraph::graph::{Graph, NodeIndex};
@@ -14,44 +14,63 @@ use petgraph::Direction;
 use std::io::stdin;
 
 use std::fs::File;
-use std::io::Read;
-
-
-fn load_file(path: &str) -> String {
-    use std::path::Path;
-    println!("file exists: {}", Path::new(path).exists());
-    let mut file = File::open(&path).unwrap();
-    let mut text = String::new();
-    file.read_to_string(&mut text).unwrap();
-    text
-}
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
 
 fn main() {
-    //TODO: нужно залогировать важные события в игре
     init_logger();
 
     const PATH: &str = "scenes-choices.graphml";
 
-    let xml_doc = load_file(PATH);
+    let xml_doc = match load_file(PATH) {
+        Ok(doc) => doc,
+        Err(e) => {
+            const MSG: &str = "Ошибка чтения файла GraphML";
+            error!("{} {:?}", MSG, e);
+            panic!(MSG);
+        }
+    };
 
-    let graph = read_graphml(&xml_doc).expect("failed read graphml"); //TODO: здесь может быть можно переделать на mdo!
+    let graph = match read_graphml(&xml_doc) {
+        Ok(graph) => graph,
+        Err(e) => {
+            const MSG: &str =
+                "Ошибка парсинга графа из GraphML формата";
+            error!("{} {:?}", MSG, e);
+            panic!(MSG);
+        }
+    };
 
     start_game(&graph);
 }
 
+fn load_file(path: &str) -> io::Result<String> {
+    println!("Файл найден: {}", Path::new(path).exists());
+
+    let mut file = File::open(&path)?;
+    let mut text = String::new();
+
+    file.read_to_string(&mut text)?;
+
+    Ok(text)
+}
+
 fn start_game(graph: &Graph<Vertex, Edge>) {
-    //TODO: заменить expect на match с error!(???)
     const EXIT_CODE: usize = 0;
 
     let mut input = String::new();
     let mut number: usize;
 
-    let mut vertex_ix: NodeIndex;
-    vertex_ix = graph
-        .node_indices()
-        .take(1)
-        .next()
-        .expect("failed get first vertex in graph");
+    let mut vertex_ix: NodeIndex = match graph.node_indices().take(1).next() {
+        Some(vertex) => vertex,
+        None => {
+            const MSG: &str =
+                "Не удалось получить первую вершину в графе";
+            error!("{}", MSG);
+            panic!(MSG);
+        }
+    };
 
     let mut out_edges: Vec<EdgeReference<'_, Edge>>;
 
@@ -96,7 +115,7 @@ fn start_game(graph: &Graph<Vertex, Edge>) {
                     trace!("out edges count: {}", out_edges.len());
                     trace!("out edges: {:?}", out_edges);
 
-                    if let Some(found_vertex_ix) = out_edges.get(usize::from(number - 1)) {
+                    if let Some(found_vertex_ix) = out_edges.get(number - 1) {
                         vertex_ix = found_vertex_ix.target();
                     } else {
                         println!("Не получилось получить вариант");
