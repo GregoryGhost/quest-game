df5546d9eea744ca2391e9a69cb2f4b16a2c722b
Gregory <lobster287@gmail.com>
2019-10-27 01:41:06 +0700

    #4 Добавил пример загрузки файла в вебе

--- web-view/src/main.rs
+++ web-view/src/main.rs
@@ -3,6 +3,10 @@
 #[path = "./lib.rs"]
 pub mod counter;
 
+#[path = "./file_upload_sample.rs"]
+pub mod file_upload;
+
 fn main() {
-    yew::start_app::<counter::SceneModel>();
+    // yew::start_app::<counter::SceneModel>();
+    yew::start_app::<file_upload::Model>();
 }
